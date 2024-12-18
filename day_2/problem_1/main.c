f70d477e8d9b0d789f582cf04f1473ab549736a2
Michael Beavitt <mbeavitt@gmail.com>
2024-12-18 15:50:20 +0000

    Added solution to day_2/problem_1

--- day_2/problem_1/main.c
+++ day_2/problem_1/main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
+
+#define MAX_STR_LEN 100
+#define ARRAY_LEN 20
+
+// Reads a line from the file pointed to by fp.
+// Returns number of entries written to array.
+int read_line(int *a, int len, FILE *fp);
+
+// reads the array, returning true if it's 'safe', false if 'unsafe'
+bool check_line(int *a, int len);
 
 int main(int argv, char **argc)
 {
+    // check that an input file was given
     if (argv != 2) {
         printf("Usage: %s <input.txt>\n", argc[0]);
         exit(EXIT_FAILURE);
     }
 
     FILE *fp;
-    int a[5];
-    int safe_reports = 0;
-    int diff;
+    int a[ARRAY_LEN];
+    int nums_read;
+
+    int safe_lines = 0;
 
     fp = fopen(argc[1], "r");
 
@@ -21,34 +34,95 @@ int main(int argv, char **argc)
         exit(EXIT_FAILURE);
     }
 
-    while (fscanf(fp, "%d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4]) == 5) {
-        bool lt         = false;
-        bool gt         = false;
-        bool eq         = false;
-        bool over_three = false;
+    // read lines in one at a time from the file, and if
+    // they are valid 'safe' lines, increment safe_lines.
+    for (;;) {
+        nums_read = read_line(a, ARRAY_LEN, fp);
 
-        for (int i = 1; i < 5; i++) {
-            if (a[i] > a[i-1]) {
-                lt = true;
-            } else if (a[i] == a[i-1]) {
-                eq = true;
-            } else {
-                gt = true;
-            }
-
-            diff = a[i] - a[i-1];
-            diff = diff < 0 ? diff * -1 : diff;
-
-            if (diff > 3) {
-                over_three = true;
-            }
-
+        if (nums_read == 0) {
+            break;
         }
 
-        if (!(over_three || (lt && gt) || eq)) {
-            safe_reports++;
+        for (int i = 0; i < nums_read; i++) {
+            printf("%d ", a[i]);
         }
-    }    
 
-    printf("Safe reports: %d\n", safe_reports);
+        if (check_line(a, nums_read) == true) {
+            safe_lines++;
+        }
+
+        printf("\n");
+    }
+
+    printf("\nSafe levels: %d\n", safe_lines);
+
+}
+
+bool check_line(int *a, int len)
+{
+    bool lt         = false;
+    bool gt         = false;
+    bool eq         = false;
+    bool over_three = false;
+
+    int diff;
+
+    for (int i = 1; i < len; i++) {
+
+        if (a[i] > a[i-1]) {
+            lt = true;
+        } else if (a[i] == a[i-1]) {
+            eq = true;
+        } else {
+            gt = true;
+        }
+
+        // difference between current and last number
+        diff = a[i] - a[i-1];
+        // converting to absolute difference
+        diff = diff < 0 ? diff * -1 : diff;
+
+        if (diff > 3) {
+            over_three = true;
+        }
+    }
+
+    if (!(over_three || (lt && gt) || eq)) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+int read_line(int *a, int len, FILE *fp)
+{
+    char str[MAX_STR_LEN];
+    char *p = str;
+    int i = 0;
+
+    // read in line from file pointer
+    if (fgets(str, sizeof(str), fp) == NULL) {
+        return 0;
+    }
+
+    // deal with any whitespace at beginning of line
+    while (isspace(*p)) {
+        p++;
+    }
+
+    // scan line into array, then increment string ptr to next digit
+    while (sscanf(p, "%d", &a[i]) != EOF) {
+        i++;
+        // p is still pointing at old digit
+        while (isdigit(*p)) {
+            p++;
+        }
+        while (isspace(*p)) {
+            p++;
+        }
+        // p should now point to next digit
+    }
+
+    return i;
 }
